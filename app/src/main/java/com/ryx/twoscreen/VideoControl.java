fc794c3c0a454778de9b854ff52b3ed225d08e43
fjtkchenhuan <13714854975@163.com>
2018-10-25 14:26:33 +0800

    双屏异显代码第一次提交

---
+++ app/src/main/java/com/ryx/twoscreen/VideoControl.java
@@ -0,0 +1,15 @@
+package com.ryx.twoscreen;
+
+import android.view.View;
+import android.widget.PopupWindow;
+
+/**
+ * Created by RYX on 2016/6/24.
+ */
+public class VideoControl extends PopupWindow {
+
+    @Override
+    public void setContentView(View contentView) {
+        super.setContentView(contentView);
+    }
+}
